313d409a00bbfd99cf58d08198b0fad8db4b74e9
Gray Reinhard <gray.reinhard@gmail.com>
2019-01-22 11:39:43 -0500

    add build folder to deploy

---
+++ build/precache-manifest.e108fe22d52a02089a02aab74cb8258b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b4c0416371638839a30",
+    "url": "/static/css/main.d87daedc.chunk.css"
+  },
+  {
+    "revision": "4b4c0416371638839a30",
+    "url": "/static/js/main.4b4c0416.chunk.js"
+  },
+  {
+    "revision": "11303aa113675a0541f7",
+    "url": "/static/js/1.11303aa1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ebef8b28c6d2abad305119ef995d3de",
+    "url": "/index.html"
+  }
+];
